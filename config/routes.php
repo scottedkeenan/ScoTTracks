bc0376db4907b36b7169816c5247734f56bb6e18
Scott <scott.keenan@thesalegroup.co.uk>
2023-01-22 09:27:30 +0000

    Flight details page WIP

--- config/routes.php
+++ config/routes.php
@@ -21,6 +21,8 @@ return function (App $app) {
 //    $app->get('countries/{country}/stats', \App\Action\CountryStatsAction::class)->setName('countrystats');
     $app->get('/stats/averages', \App\Action\StatsAveragesAction::class)->setName('averages');
 
+    $app->get('/flight/detail', \App\Action\FlightDetailAction::class)->setName('detail');
+
 };
 
 
