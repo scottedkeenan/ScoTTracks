723471118635d7c4e261e820cdfad7f91edc6ae9
Scott <scott.keenan@thesalegroup.co.uk>
2024-01-26 12:46:39 +0000

    Removal of checks for takeoff airfield type 'unknown'

--- src/Domain/Scottracks/Repository/DailyFlightsRepository.php
+++ src/Domain/Scottracks/Repository/DailyFlightsRepository.php
@@ -75,7 +75,6 @@ class DailyFlightsRepository
                     FROM daily_flights
                     INNER JOIN airfields a ON daily_flights.takeoff_airfield = a.id
                     WHERE takeoff_airfield IS NOT NULL
-                    AND takeoff_airfield != 'unknown'
                 ";
         $result = [];
         $data = $this->connection->query($query)->fetchAll();
@@ -94,7 +93,6 @@ class DailyFlightsRepository
 //                    FROM daily_flights
 //                    INNER JOIN airfields ON daily_flights.takeoff_airfield = airfields.name
 //                    WHERE takeoff_airfield IS NOT NULL
-//                    AND takeoff_airfield != 'unknown'
 //                ";
 //        $data = $this->connection->query($query)->fetchAll();
 //
@@ -114,7 +112,6 @@ class DailyFlightsRepository
                     FROM daily_flights
                     INNER JOIN airfields a ON daily_flights.takeoff_airfield = a.id
                     WHERE takeoff_airfield IS NOT NULL
-                    AND takeoff_airfield != 'unknown'
                 ";
 
         $result = [];
@@ -136,7 +133,6 @@ class DailyFlightsRepository
                     FROM daily_flights
                     INNER JOIN airfields a ON daily_flights.takeoff_airfield = a.id
                     WHERE takeoff_airfield IS NOT NULL
-                    AND takeoff_airfield != 'unknown'
                     AND daily_flights.takeoff_timestamp > current_date
                     GROUP BY takeoff_airfield
                     ORDER BY name;
@@ -159,7 +155,6 @@ class DailyFlightsRepository
                     FROM daily_flights
                     INNER JOIN airfields a ON daily_flights.takeoff_airfield = a.id
                     WHERE takeoff_airfield IS NOT NULL
-                    AND takeoff_airfield != 'unknown'
                     AND country_code = '$countryCode'
                     ORDER BY name;
                 ";
@@ -185,7 +180,6 @@ class DailyFlightsRepository
                     FROM daily_flights
                     INNER JOIN airfields ON daily_flights.takeoff_airfield = airfields.id
                     WHERE takeoff_airfield IS NOT NULL
-                    AND takeoff_airfield != 'unknown'
                     AND daily_flights.takeoff_timestamp BETWEEN $insertDate AND $insertDate + INTERVAL 1 DAY
                     AND country_code = '$countryCode'
                     GROUP BY takeoff_airfield
