382054d4b03259ec5af8fb950d03f69b52173e48
Scott <scott.keenan@thesalegroup.co.uk>
2024-01-30 18:11:33 +0000

    Conflict resolution

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -14,7 +14,54 @@ $containerBuilder->addDefinitions(__DIR__ . '/container.php');
 $container = $containerBuilder->build();
 
 // Create App instance
-$app = $container->get(App::class);
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+// Set custom error handler
+// $app->addErrorMiddleware(false, false, false);
+
+$customErrorHandler = function (
+    \Slim\Psr7\Request $request,
+    \Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails
+) use ($app) {
+    // Log the error if needed
+    if ($logErrors) {
+        // Log the error using your preferred logging mechanism
+    }
+
+    // Customize the error response
+    $response = $app->getResponseFactory()->createResponse();
+//    $response->getBody()->write("You've landed out! (An error occurred). Please try again later.");
+
+     // Extract information from the exception
+     $message = $exception->getMessage();
+     $statusCode = $exception->getCode();
+     $file = $exception->getFile();
+     $line = $exception->getLine();
+     $stackTrace = $exception->getTraceAsString();
+
+     // Prepare a readable error message
+     $errorDetails = "Message: $message\n";
+     $errorDetails .= "Code: $statusCode\n";
+     $errorDetails .= "File: $file\n";
+     $errorDetails .= "Line: $line\n";
+     $errorDetails .= "Stack Trace:\n$stackTrace\n";
+
+     // Write the error details to the response body
+     $response->getBody()->write($errorDetails);
+
+
+    // Set the content type and return the response with an appropriate status code
+    return $response
+        ->withHeader('Content-Type', 'text/plain')
+        ->withStatus(500);
+};
+
+$app->addErrorMiddleware(true, false, false)->setDefaultErrorHandler($customErrorHandler);
 
 // Register routes
 (require __DIR__ . '/routes.php')($app);
---
+++ templates/scottracks/stats.php
@@ -0,0 +1,124 @@
+<!doctype html>
+
+<?php date_default_timezone_set('Europe/London'); ?>
+
+<html lang="en">
+<head>
+    <?php include('includes/head.phtml'); ?>
+</head>
+<body>
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <?php include('includes/sidebar.php'); ?>
+
+        <div id="content">
+
+            <!-- Navbar -->
+            <?php include('includes/navbar.php'); ?>
+            <?php
+            $topLaunchersData = $data['top_launchers'];
+            $differenceData = $data['difference'];
+            ?>
+
+                <h5>Top airfields this week</h5>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-sm w-auto">
+                        <thead>
+                        <tr>
+                            <th scope="col" class="col-xs-4">Airfield</th>
+                            <th scope="col" class="col-xs-4">Launches this week</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($topLaunchersData as $top_launcher) : ?>
+                                <tr>
+                                    <td><a href="<?php echo sprintf('/airfields/%s',$top_launcher['id'])?>"> <?php echo $top_launcher['name'] ?></a></td>
+                                    <td><?php echo $top_launcher['flight_count']?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            <h5><?php
+                $secondWeekDate = new DateTime($data['difference']['week_start_date']);
+                $firstWeekDate = clone $secondWeekDate;
+                $firstWeekDate->modify('-7 days');
+                echo sprintf('Change in launches Week on Week %s / %s',
+                    $firstWeekDate->format('d-m-Y'),
+                    $secondWeekDate->format('d-m-Y')); ?></h5>
+                <div class="row m-1">
+                    <div class="float-left p-1">
+                        <h6>Increases</h6>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-sm w-auto">
+                                <thead>
+                                <tr>
+                                    <th scope="col" class="col-xs-4">Airfield</th>
+                                    <th scope="col" class="col-xs-4">Difference</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($data['difference']['week_on_week_pos'] as $difference) : ?>
+                                        <tr>
+                                            <td><a href="<?php echo sprintf('/airfields/%s',$difference['id'])?>"> <?php echo $difference['name'] ?></a></td>
+                                            <td><?php echo $difference['difference']?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="float-left p-1">
+                        <h6>Decreases</h6>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-sm w-auto">
+                                <thead>
+                                <tr>
+                                    <th scope="col" class="col-xs-4">Airfield</th>
+                                    <th scope="col" class="col-xs-4">Difference</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($data['difference']['week_on_week_neg'] as $difference) : ?>
+                                        <tr>
+                                            <td><a href="<?php echo sprintf('/airfields/%s',$difference['id'])?>"> <?php echo $difference['name'] ?></a></td>
+                                            <td><?php echo $difference['difference']?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                    <h5><?php
+                        $startDate = new DateTime($data['difference']['week_start_date']);
+                        echo sprintf('Flight times for week starting %s',
+                            $startDate->format('d-m-Y'))?></h5>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-sm w-auto">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="col-xs-4">Airfield</th>
+                                <th scope="col" class="col-xs-4">Total Flight Time</th>
+                                <th scope="col" class="col-xs-4">Number of Aircraft</th>
+                                <th scope="col" class="col-xs-4">Minutes per Aircraft</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($data['flight_times'] as $flight_times) : ?>
+                                    <tr>
+                                        <td><a href="<?php echo sprintf('/airfields/%s',$flight_times['id'])?>"> <?php echo $flight_times['name'] ?></a></td>
+                                        <td><?php echo $flight_times['total_flight_time']?></td>
+                                        <td><?php echo $flight_times['aircraft_count']?></td>
+                                        <td><?php echo round($flight_times['minutes_per_aircaft'])?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+        </div>
+    </div>
+    <?php include('includes/footer.html'); ?>
+    <?php include('includes/navbar_toggle.php'); ?>
+</body>
+</html>
